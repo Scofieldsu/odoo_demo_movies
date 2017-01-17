f9d7166be65fde894ef0b9b162230095945397a6
俞元 <scofieldyu@yuyuandeAir.lan>
2017-01-18 01:00:20 +0800

    初始化电影demo模块

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "YU Movies",
+
+    'summary': """
+        """,
+
+    'description': """
+        俞元的电影demo模块
+    """,
+
+    'author': "",
+    'website': "https://scofieldsu.github.io/",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'YU',
+    'version': '0.1',
+    'sequence': 1000,
+    # any module necessary for this one to work correctly
+    'depends': [ ],
+
+    # always loaded
+    'data': [
+       'views/index.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'qweb': [
+        
+    ]
+}
